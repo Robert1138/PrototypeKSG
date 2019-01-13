97c228b2362258b6d26c6071d75148af91e91c2a
Robert Wooten <robert.wooten.833@my.csun.edu>
2019-01-13 12:24:46 -0800

    added toolbar and jsPDF

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,14 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MatInputModule } from '@angular/material';
+import { MatIconModule } from '@angular/material';
+
+
+//f0ead6
+
+import { RoutingModule } from './routing.module';
 
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { FormPageComponent } from './form-page/form-page.component';
+import { AboutComponent } from './about/about.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    FormPageComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    MatStepperModule,
+    RoutingModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ ClientApp/src/app/form-page/form-page.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import * as  jspdf from 'jspdf';
+import { stringify } from '@angular/core/src/util';
+import { text } from '@angular/core/src/render3';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+
+@Component({
+  selector: 'app-form-page',
+  templateUrl: './form-page.component.html',
+  styleUrls: ['./form-page.component.css']
+})
+export class FormPageComponent implements OnInit {
+
+  
+  test: string;
+
+  isLinear = false;
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+
+  constructor(private _formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+
+    this.firstFormGroup = this._formBuilder.group({
+      firstCtrl: ['', Validators.required]
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required]
+    });
+
+
+
+  }
+
+
+createPDF(): void {
+console.log("got here ");
+  var doc = new jspdf();
+
+  let baseText: string;
+
+  baseText = 'I, Awesome Mom, make this Declaration to appoint the guardian for my children, listed as \n' + 
+  'follows, in the event of my death or incapacity:\n' ;
+
+ // console.log(doc.getFontList);
+doc.setFontStyle("bold");
+doc.setFontSize(18);
+doc.setFont("arial");  
+doc.text('       Declaration of Appointment of Guardian for My', 20, 20);
+doc.text('    My Children in the Event of My Death or Incapacity', 20, 30);
+doc.text(baseText, 20, 40);
+
+doc.save('a4.pdf');
+
+}
+
+
+
+
+}
