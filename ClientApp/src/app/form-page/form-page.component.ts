96aa167d49838f228596aeaac95c2a7108d9d543
Robert Wooten <robert.wooten.833@my.csun.edu>
2019-01-13 22:01:09 -0800

    pdf form creation can be demoed

--- ClientApp/src/app/form-page/form-page.component.ts
+++ ClientApp/src/app/form-page/form-page.component.ts
@@ -35,9 +35,23 @@ export class FormPageComponent implements OnInit {
   }
 
 
+showOutput(): void {
+console.log(this.firstFormGroup.get('firstCtrl').value);
+console.log(this.secondFormGroup.get('secondCtrl').value);
+}
+
+
+
+
+
 createPDF(): void {
 console.log("got here ");
-  var doc = new jspdf();
+let name: string;
+let address: string;  
+name = this.firstFormGroup.get('firstCtrl').value
+address = this.secondFormGroup.get('secondCtrl').value
+
+var doc = new jspdf();
 
   let baseText: string;
 
@@ -50,7 +64,12 @@ doc.setFontSize(18);
 doc.setFont("arial");  
 doc.text('       Declaration of Appointment of Guardian for My', 20, 20);
 doc.text('    My Children in the Event of My Death or Incapacity', 20, 30);
+doc.setFontSize(12);
+doc.setFont("times new roman");  
+
 doc.text(baseText, 20, 40);
+doc.text(name + "\n" + address, 20, 50);
+
 
 doc.save('a4.pdf');
 
